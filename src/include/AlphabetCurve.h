25a34a3635e7319a49194ff956510e3a95b8dc9f
Asanilta Fahda <asanilta@gmail.com>
2016-03-20 12:48:46 +0700

    fix alphabetcurve.h

--- src/include/AlphabetCurve.h
+++ src/include/AlphabetCurve.h
@@ -1,5 +1,5 @@
-#ifndef Alphabet_h
-#define Alphabet_h
+#ifndef AlphabetCurve_h
+#define AlphabetCurve_h
 
 #include "CurveCollection.h"
 #include <map>
@@ -8,11 +8,9 @@
 
 #include <fstream>
 
-class Alphabet {
+class AlphabetCurve {
 	public:
-	Alphabet();
-	Alphabet(std::ifstream& file);
-	void initAlphabet();
+	AlphabetCurve(std::ifstream& file);
 	Letter getLetter(char c);
 
 	void drawChar(char a, int X, int Y, float scale);
